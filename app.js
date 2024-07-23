fba053c4de589aa5082f86936e8fd9f4121b2697
LindelGitGut <alexandre.lindele@outlook.de>
2024-07-23 10:51:41 +0200

    continued with mouse events

--- app.js
+++ app.js
@@ -2,16 +2,32 @@
 const app = Vue.createApp({
     // data ist eine reservierte Funktion, um Werte für eine Vue Komponente zurückzugeben,
     // muss eine Funktion sein, die Objekte zurückgibt
+    //einezelne Elemente werden durch Komma getrennt
+    // in Objekten wird mit : der Key definiert
     data() {
       return {
         title: "Hans Wurst der Knochenknecht",
         author: "Der Mitternachts Würstchenmann",
-        age: 42
+        age: 42,
+        showBook: true
       };
     },
     // Beispiel für Template, HTML muss als String übergeben werden,
     // das Template wird im DOM an entsprechender ID übergeben, hier setzen wir mit {{attributname}} die referenz zu den data attributen
-    template: "<h2> I am a template!</h2><br>title: {{ title }}"
+    template: "<h2> I am a template!</h2><p>title: {{ title }}</p> <p> author: {{author}}</p><p>age: {{age}} <br> <button v-on:click='age++'>Increase Age</button><button v-on:click='age--'>Decrease Age</button><button v-on:click='changeBookTitle'>Change Book Title</button><div v-if='age >= 50'> You are at least half a century old.</div><button v-on:click='toggleShowBook'><span v-if='showBook'>Hide Book</span><span v-if='!showBook'>Show Book</span></button><br><p v-if='showBook'>{{title}}</p></div>"
+,
+
+    //durch Methods: werden Methoden für die entsprechende Komponente bereitgestellt
+
+    methods: {
+        changeBookTitle(){
+            this.title = "This is the new Title!"
+        },
+        toggleShowBook(){
+            this.showBook = !this.showBook;
+        }
+    }
+
   });
   
   // App mounten, hier mounten wir die App, wir müssen dafür einen Bezeichner als Parameter übergeben,
---
+++ mouseevent.js
@@ -0,0 +1,42 @@
+const mouseventapp = Vue.createApp({
+
+data(){
+    return {
+        x: 0,
+        y: 0
+    };},
+
+    methods:
+
+{
+    mouseOverEvent(){
+        console.log("Mouseover");
+    },
+
+    mouseLeaveEvent(){
+        console.log("Mouseleave");
+    },
+
+    doubleClickEvent(){
+        console.log("Double Click");
+    },
+
+    //Beispiel um das Event selbst als Parameter zu verwenden
+    mouseMoveEvent(event){
+        console.log("Mousemove");
+        console.log(event);
+
+    //wir nehmen die x und y koordinaten des Mauszeigers innerhalb der box und schreiben diese in data als attribut um später in der box anzuzeigen
+        this.x = event.offsetX;
+        this.y = event.offsetY;
+    },
+}
+
+
+
+
+
+
+
+
+}).mount("#mouseeventapp");
